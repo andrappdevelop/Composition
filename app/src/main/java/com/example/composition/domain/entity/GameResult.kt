90ce0eeaf1e418356ac547464a3e3dd013fe0f03
Ivan Usatov <andrappdevelop@gmail.com>
2023-01-17 15:08:20 +0700

    Presentation : Replace serializable to parcelable, add buttonListener on GameFinishedFragment.kt

--- app/src/main/java/com/example/composition/domain/entity/GameResult.kt
+++ app/src/main/java/com/example/composition/domain/entity/GameResult.kt
@@ -1,10 +1,12 @@
 package com.example.composition.domain.entity
 
-import java.io.Serializable
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-data class GameResult (
+@Parcelize
+data class GameResult(
     val isWin: Boolean,
     val countOfRightAnswers: Int,
     val countOfQuestions: Int,
     val gameSettings: GameSettings
-) : Serializable
+) : Parcelable
